091a297126833c0a4e736593df3679eff3158eb4
therealgg13 <zhaoyu.guo@mail.utoronto.ca>
2022-02-19 16:07:53 -0500

    Added L1 file

---
+++ lesson.py
@@ -0,0 +1,25 @@
+"""
+CSTWPY 
+Python for Beginners 
+
+=== MODULE DESCRIPTION === 
+This module contains all the code for tutorial 1.
+
+Copyright (C) 2022 therealgg13.
+"""
+
+# declaring variables 
+x = 4
+
+# reassign variables 
+x = 6 + 3 
+
+# referencing an non-existing variable 
+y
+# What do you think would return?
+
+# adding 
+x = 4 + 3
+
+# adding two strings 
+y = "I love doing" + " this"
