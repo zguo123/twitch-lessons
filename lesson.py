2ece4411d4a4265c9f451966dc6757af31026f55
therealgg13 <zhaoyu.guo@mail.utoronto.ca>
2022-02-20 17:23:48 -0500

    Updated lesson - simplified

--- lesson.py
+++ lesson.py
@@ -8,9 +8,18 @@ This module contains all the code for tutorial 1.
 Copyright (C) 2022 therealgg13.
 """
 
+
+sample_string = "breadgoesbrrr" # string 
+sample_float = 0.03 # float 
+sample_int = 90 # integer 
+sample_boolean = True # boolean (True, False)
+sample_list = ["Hello Toki Club", "Hello MsKitty"]
+# ... many others
+
 # declaring variables 
 x = [i for i in range(4)]
 y = x + [1]
+
 
 y = "2"
 y = (2,3, 4)
@@ -18,7 +27,6 @@ y = (2,3, 4)
 
 def bar(x: str) -> str: 
     return x + "rgrg"
-
 
 # reassign variables 
 x = 6 + 3 
@@ -31,3 +39,18 @@ x = 6 + 3
 x = 4 + 3
 
 
+
+NAME = 'therealgg13'
+AGE = 21
+isMajority = AGE > 18
+friends = []
+
+def addFriend(friend: str) -> None: 
+    friends.append(friend)
+
+
+def formatNice() -> str: 
+    return f'{NAME} {AGE}'
+
+
+addFriend("Mskitty")
