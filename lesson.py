0cdca6f3b64d4f16a94ffe9e0b1be5e09d13567d
therealgg13 <zhaoyu.guo@mail.utoronto.ca>
2022-02-19 20:31:56 -0500

    Added written lessons

--- lesson.py
+++ lesson.py
@@ -9,18 +9,25 @@ Copyright (C) 2022 therealgg13.
 """
 
 # declaring variables 
-x = 4
+x = [i for i in range(4)]
+y = x + [1]
+
+y = "2"
+y = (2,3, 4)
+
+
+def bar(x: str) -> str: 
+    return x + "rgrg"
 
 
 # reassign variables 
 x = 6 + 3 
 
 # referencing an non-existing variable 
-y
+
 # What do you think would return?
 
 # adding 
 x = 4 + 3
 
-# adding two strings 
-y = "I love doing" + " this"
+
